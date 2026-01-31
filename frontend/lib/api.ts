df3267fa31f9fe0bb648619d42b8280d1edb52f7
Sandip Katel <babusandipkatel@gmail.com>
2026-01-31 19:12:05 +0000

    fix: update env var name

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // For server-side API calls
 const apiBaseUrl =
-  process.env.NEXT_BACKEND_URL || "http://localhost:8000/api/v1"; // Browser environment
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1"; // Browser environment
 // Create axios instance
 const api = axios.create({
   baseURL: apiBaseUrl,
